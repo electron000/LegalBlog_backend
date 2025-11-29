816779e54d6b97b7b6ea2d0450f638c1f577b785
electron000 <ajarunchang@gmail.com>
2025-11-30 00:51:04 +0530

    Force update

--- main.py
+++ main.py
@@ -31,8 +31,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://ai-legalmate.vercel.app", 
-        "http://127.0.0.1:5173", 
-        "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -46,4 +44,4 @@ def read_root():
     return {"message": "Welcome to the Indian Law Blog Generator AI"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
